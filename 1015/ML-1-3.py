c2b391455b8ca98adedc519df4b8e0db90ab67fc
Fixer <67309088+leelang7@users.noreply.github.com>
2024-10-15 20:34:01 +0900

    Create ML-1-3.py

---
+++ 1015/ML-1-3.py
@@ -0,0 +1,16 @@
+from sklearn import svm
+
+def train_model(x_mat, y_vec):
+    
+    # <ToDo>: scikit-learn을 활용해서 모델을 생성하고, x_mat, y_vec으로 모델을 학습시킵니다.
+    model = None
+    trained_model = None
+    
+    return trained_model
+
+def evaluate_model(model, x_mat, y_vec):
+    
+    # <ToDo>: 검증용으로 주어진 데이터를 이용해서 모델의 성능을 평가합니다.
+    mean_acc = None
+    
+    return mean_acc
