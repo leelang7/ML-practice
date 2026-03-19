a6794f111362942d4467c2c64e09eb26592db2b4
All that AI <67309088+leelang7@users.noreply.github.com>
2026-03-19 13:06:41 +0900

    update

--- ML2/sol_q4/main.py
+++ ML2/sol_q4/main.py
@@ -1,4 +1,5 @@
-# main.py
+from elice_utils import EliceUtils
+elice_utils = EliceUtils()
 import warnings 
 warnings.simplefilter("ignore")
 import preprocess
@@ -7,13 +8,13 @@ import graph_plot
 
 def main():
     # 데이터를 불러옵니다.
-    x_train, y_train, x_val, y_val = preprocess.load_data('./iris.csv', independent_var = ['petal length', 'sepal length'], response_var = 'class_num')
+    x_train, y_train, x_val, y_val = preprocess.load_data('./data/iris.csv', independent_var = ['petal length', 'sepal length'], response_var = 'class_num')
     
     # <ToDo>: svm.py 안의 함수를 사용해 네 SVM 모델을 불러오고 학습시킵니다.
-    svm_linear = svm.train_linear_model(x_train, y_train)
-    svm_poly = svm.train_poly_model(x_train, y_train)
-    svm_rbf = svm.train_rbf_model(x_train, y_train)
-    svm_sig = svm.train_sig_model(x_train, y_train)
+    svm_linear = None
+    svm_poly = None
+    svm_rbf = None
+    svm_sig = None
     
     print("Independent variable: {}".format("petal length" + ' and ' + 'sepal length'))
     
@@ -21,10 +22,10 @@ def main():
     graph_plot.svm_model_plot(svm_linear, svm_poly, svm_rbf, svm_sig, x_train, y_train, feature_name = ['petal length', 'sepal length'] )
     
     # <ToDo>: svm.py 안의 함수를 사용해, 학습된 네 모델의 성능을 정확도로써 측정합니다.
-    mean_acc_linear = svm.evaluate_model(svm_linear, x_val, y_val)
-    mean_acc_poly = svm.evaluate_model(svm_poly, x_val, y_val)
-    mean_acc_rbf = svm.evaluate_model(svm_rbf, x_val, y_val)
-    mean_acc_sig = svm.evaluate_model(svm_sig, x_val, y_val)
+    mean_acc_linear = None
+    mean_acc_poly = None
+    mean_acc_rbf = None
+    mean_acc_sig = None
     
     print("mean accuracy for linear kernel: {}%".format(mean_acc_linear*100))
     print("mean accuracy for poly kernel: {}%".format(mean_acc_poly*100))
